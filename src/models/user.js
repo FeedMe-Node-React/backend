baca02dd284f9a58c5aa78249d9a3956083d2ab5
Phil DeLong <philjdelong@gmail.com>
2020-09-30 17:46:35 -0600

    Add timestamps to user

--- src/models/user.js
+++ src/models/user.js
@@ -1,27 +1,30 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
-const User = new Schema ({
-    email: {
-        type: String,
-        required: true,
+const UserSchema = new Schema (
+    {
+        email: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        status: {
+            type: String,
+            default: "I'm a new member!"
+        },
+        posts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Post',
+        }],
     },
-    password: {
-        type: String,
-        required: true,
-    },
-    name: {
-        type: String,
-        required: true,
-    },
-    status: {
-        type: String,
-        required: true,
-    },
-    posts: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Post',
-    }]
-});
+    { timestamps: true },
+);
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model('User', UserSchema)
