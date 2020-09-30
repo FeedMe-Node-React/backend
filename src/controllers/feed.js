ba102afda5e5f7d8538890511341251a132ce912
Phil DeLong <philjdelong@gmail.com>
2020-09-30 17:47:19 -0600

    Update syntax

--- src/controllers/feed.js
+++ src/controllers/feed.js
@@ -1,4 +1,3 @@
-const post = require('../models/post');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
@@ -38,49 +37,49 @@ exports.createPost = (req, res, next) => {
     });
   };
   
-  exports.getPost = (req, res, next) => {
-    const postId = req.params.postId;
-    Post
-      .findById(postId)
-      .then(result => {
-        res
-          .status(200)
-          .json({
-            post: result
-          })
-      })
-      .catch(err => {
-        console.log(err)
-      });
-    }
-    
-    exports.editPost = (req, res, next) => {
-      const postId = req.params.postId;
-      Post
-      .findByIdAndUpdate(postId)
-      .then(result => {
-        result.title = req.body.title
-        result.content = req.body.content
-        result.save()
-        res
-          .status(200)
-          .json({
-            post: result
-          })
-      })
-      .catch(err => {
-        console.log(err)
-      });
-  };
-
-  exports.deletePost = (req, res, next) => {
-    const postId = req.params.postId;
-    console.log(postId)
-    Post
-      .findByIdAndDelete(postId)
-      .then(result => {
-        res
-          .status(200)
-          // .json({post: result})
-      })
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post
+    .findById(postId)
+    .then(result => {
+      res
+        .status(200)
+        .json({
+          post: result
+        })
+    })
+    .catch(err => {
+      console.log(err)
+    });
   }
+    
+exports.editPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post
+  .findByIdAndUpdate(postId)
+  .then(result => {
+    result.title = req.body.title
+    result.content = req.body.content
+    result.save()
+    res
+      .status(200)
+      .json({
+        post: result
+      })
+  })
+  .catch(err => {
+    console.log(err)
+  });
+};
+
+exports.deletePost = (req, res, next) => {
+  const postId = req.params.postId;
+  console.log(postId)
+  Post
+    .findByIdAndDelete(postId)
+    .then(result => {
+      res
+        .status(200)
+        // .json({post: result})
+    })
+};
