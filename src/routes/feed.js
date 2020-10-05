bbd12b64319f575777123858515cc751322e0df4
Phil DeLong <philjdelong@gmail.com>
2020-10-05 12:36:44 -0600

    Add jwt middleware to feed routes

--- src/routes/feed.js
+++ src/routes/feed.js
@@ -2,20 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 const feedController = require('../controllers/feed');
+const jwtAuth = require('../utils/jwtAuth')
+
 
 // GET /feed/posts
-router.get('/posts', feedController.getPosts);
+router.get('/posts', jwtAuth, feedController.getPosts);
 
 // POST /feed/post
-router.post('/post', feedController.createPost);
+router.post('/post', jwtAuth, feedController.createPost);
 
 // GET /feed/post/:postId
-router.get('/post/:postId', feedController.getPost);
+router.get('/post/:postId', jwtAuth, feedController.getPost);
 
 // PATCH /feed/post/:postId
-router.patch('/post/:postId', feedController.editPost);
+router.patch('/post/:postId', jwtAuth, feedController.editPost);
 
 // DELETE /feed/post/:postId
-router.delete('/post/:postId', feedController.deletePost);
+router.delete('/post/:postId', jwtAuth, feedController.deletePost);
 
 module.exports = router;
