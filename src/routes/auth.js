b178d5fd284e5546212cc8b42f502d4a0c2fdddc
Phil DeLong <philjdelong@gmail.com>
2020-10-16 13:50:05 -0600

    Remove comments

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,10 +3,7 @@ import authController from '../controllers/auth';
 
 const router = express.Router();
 
-// CREATE User
 router.post('/signup', authController.userSignup);
-
-// LOGIN User
 router.post('/login', authController.userLogin);
 
 module.exports = router;
--- src/routes/feed.js
+++ src/routes/feed.js
@@ -4,19 +4,14 @@ const router = express.Router();
 import feedController from '../controllers/feed';
 import jwtAuth from '../utils/jwtAuth';
 
-// GET /feed/posts
 router.get('/posts', jwtAuth, feedController.getPosts);
 
-// POST /feed/post
 router.post('/post', jwtAuth, feedController.createPost);
 
-// GET /feed/post/:postId
 router.get('/post/:postId', jwtAuth, feedController.getPost);
 
-// PATCH /feed/post/:postId
 router.patch('/post/:postId', jwtAuth, feedController.editPost);
 
-// DELETE /feed/post/:postId
 router.delete('/post/:postId', jwtAuth, feedController.deletePost);
 
 module.exports = router;
