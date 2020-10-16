e7c3e0ddbd9445557e42222e882b5df78771ba56
Phil DeLong <philjdelong@gmail.com>
2020-10-16 13:52:17 -0600

    Clean up syntax

--- src/routes/feed.js
+++ src/routes/feed.js
@@ -1,17 +1,13 @@
 import express from 'express';
-const router = express.Router();
-
 import feedController from '../controllers/feed';
 import jwtAuth from '../utils/jwtAuth';
 
+const router = express.Router();
+
 router.get('/posts', jwtAuth, feedController.getPosts);
-
 router.post('/post', jwtAuth, feedController.createPost);
-
 router.get('/post/:postId', jwtAuth, feedController.getPost);
-
 router.patch('/post/:postId', jwtAuth, feedController.editPost);
-
 router.delete('/post/:postId', jwtAuth, feedController.deletePost);
 
 module.exports = router;
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,10 @@
 import express from 'express';
-const router = express.Router();
 import userController from '../controllers/user';
 import jwtAuth from '../utils/jwtAuth';
 
-router.get('/dashboard', jwtAuth, userController.getDashboard);
+const router = express.Router();
 
+router.get('/dashboard', jwtAuth, userController.getDashboard);
 router.post('/:userId', userController.getStatus);
 router.patch('/:userId', userController.updateStatus);
 
