fabb551a9286c320c622cf3f976eb9ada59017cf
Phil DeLong <philjdelong@gmail.com>
2020-10-15 18:23:31 -0600

    Add babelrc

--- src/utils/imageUploader.js
+++ src/utils/imageUploader.js
@@ -2,7 +2,7 @@ import multer from 'multer';
 
 const fileStorage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'src/images');
+        cb(null, 'dist/images');
     },
     filename: (req, file, cb) => {
         cb(null, new Date().toISOString() + '-' + file.originalname);
