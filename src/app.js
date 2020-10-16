d7b791869f9a762a30b694096da626d2f49172ab
Phil DeLong <philjdelong@gmail.com>
2020-10-16 09:13:58 -0600

    Update build to create image direrctory

--- src/app.js
+++ src/app.js
@@ -3,18 +3,16 @@ import multer from 'multer';
 import path from 'path'
 import express from 'express';
 import mongoose from 'mongoose';
-
 import dotenv from 'dotenv';
-dotenv.config();
-const app = express();
-
 import feedRoutes from './routes/feed';
 import authRoutes from './routes/auth';
 import userRoutes from './routes/user';
-
 // import io from './utils/openSocket';
 // import './utils/dbConnect';
 // import './utils/imageUploader';
+
+dotenv.config();
+const app = express();
 
 const MongoConnect = async (error) => {
     try {
@@ -23,12 +21,12 @@ const MongoConnect = async (error) => {
             useNewUrlParser: true, 
             useCreateIndex: true 
         });
+        console.log('DB Connected')
         // const server = app.listen(8080 || process.env.HEROKU_URI);
         // io.init(server);
         // io.on('connection', socket => {
             // console.log('DB Connected')
         // })
-        console.log('DB Connected')
     } catch (error) {
         console.log(error);
     }
@@ -37,11 +35,10 @@ MongoConnect();
 
 const fileStorage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'src/images');
+        cb(null, 'dist/images');
     },
     filename: (req, file, cb) => {
-        // cb(null, new Date().toISOString() + '-' + file.originalname);
-        cb(null, file.originalname);
+        cb(null, new Date().toISOString() + '-' + file.originalname);
     }
 });
 
@@ -57,23 +54,17 @@ const fileFilter = (req, file, cb) => {
     }
 };
 
-app.use(
-    multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
-);
-    
-app.use('/src/images', express.static(path.join(__dirname, 'images')));
-    
-app.use(bodyParser.json());
-
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-        'Access-Control-Allow-Methods',
-        'OPTIONS, GET, POST, PUT, PATCH, DELETE'
-    );
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
     next();
 });
+
+app.use(bodyParser.json());
+
+app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'));    
+app.use('/dist/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/feed', feedRoutes);
 app.use('/auth', authRoutes);
